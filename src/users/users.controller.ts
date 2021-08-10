14e3b4aa9df65e89ee2aefb6e008239a30afdc6a
Dmitri <d.buryak@gmail.com>
2021-08-10 22:55:34 +0300

    set HttpCode

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { User } from './user.model';
-import { find } from 'rxjs';
+import { Users } from './users.entity';
 
 @Controller('login')
 export class UsersController {
   @Post()
-  find(@Body() user: User): string {
-    return user.login;
+  // @HttpCode(204)
+  async authorize(@Body() user: User): Promise<any> {
+    const userToAuthorize = await Users.findOne({
+      login: user.login,
+      password: user.password,
+    });
+    userToAuthorize.token = Date.now().toString();
+    console.log(userToAuthorize);
+    if (userToAuthorize) {
+      await userToAuthorize.save();
+      return userToAuthorize.token;
+    }
+    return;
   }
 }
